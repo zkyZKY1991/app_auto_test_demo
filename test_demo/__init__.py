77e59bc62f47020d7db4e0c17814bbdea3616bbf
ZKY <475854688@qq.com>
2020-04-26 15:14:17 +0800

    提价测试demo项

---
+++ test_demo/__init__.py
@@ -0,0 +1,6 @@
+# -*-coding:utf-8-*-
+# @Time:2020/4/26   14:38
+# @Author:ZKY
+# @Email:475854688@qq.com
+# @File:__init__.py.py
+# @Software:PyCharm
---
+++ test_demo/test_activity.py
@@ -0,0 +1,6 @@
+# -*-coding:utf-8-*-
+# @Time:2020/4/26   14:38
+# @Author:ZKY
+# @Email:475854688@qq.com
+# @File:test_activity.py
+# @Software:PyCharm
