e4c935dfa17c9364d7716affcf5710218196ffa2
ZKY <475854688@qq.com>
2020-04-26 18:22:57 +0800

    提交修改

--- test_demo/test_activity.py
+++ test_demo/test_activity.py
@@ -4,3 +4,24 @@
 # @Email:475854688@qq.com
 # @File:test_activity.py
 # @Software:PyCharm
+from appium import webdriver
+
+desired_capabilities ={
+    # 添加测试机的设备名，android此项可以随便写
+    "deviceName":"127.0.0.1:62001",
+    # 添加设备的Android版本
+    "platformVersion":"5.1.1",
+    # 添加测试平台Android/ios
+    "platformName":"Android",
+    # 添加被测的APP包名
+    "appPackage":"com.mobile.videonews.li.video",
+    # 添加测试APP的启动入口
+    "appActivity":"com.mobile.videonews.li.video.act.StartAty",
+    # 设置测试框架为uiautomator2
+    "automationName":"uiautomator2",
+    # 设置不需要每次都是第一次启动app
+    "noRest":"True"
+}
+
+driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub",desired_capabilities)
+
